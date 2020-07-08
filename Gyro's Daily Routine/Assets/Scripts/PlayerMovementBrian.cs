450eb84fe741b696146d5a7ff2de24238650b4f6
BrianWhitePerth <61271275+BrianWhitePerth@users.noreply.github.com>
2020-07-08 12:47:30 +0800

    player functionality updated

--- Gyro's Daily Routine/Assets/Scripts/PlayerMovementBrian.cs
+++ Gyro's Daily Routine/Assets/Scripts/PlayerMovementBrian.cs
@@ -4,23 +4,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
+public class PlayerMovementBrian : MonoBehaviour
 {
-    public int moveSpeed;
+    public float moveSpeed;
+    public float jumpSpeed;
     private float horizontalInput;
 
+
     Rigidbody2D myRigidbody2D;
+    CapsuleCollider2D myBodyCollider;
+    BoxCollider2D myFeet;
 
     // Start is called before the first frame update
     void Start()
     {
-        myRigidbody2D = GetComponent<Rigidbody2D>();  
+        myRigidbody2D = GetComponent<Rigidbody2D>();
+        myBodyCollider = GetComponent<CapsuleCollider2D>();
+        myFeet = GetComponent<BoxCollider2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
         Run();
+        Jump();
         FlipSprite();
         //Move();
     }
@@ -30,12 +37,20 @@ public class PlayerMovement : MonoBehaviour
         float moveCharacter = Input.GetAxis("Horizontal");
         Vector2 playerVelocity = new Vector2(moveCharacter * moveSpeed, myRigidbody2D.velocity.y);
         myRigidbody2D.velocity = playerVelocity;
+        Debug.Log(playerVelocity);
     }
 
-    private void Move()
+    private void Jump()
     {
-        horizontalInput = Input.GetAxis("Horizontal");
-        transform.Translate(Vector2.right * Time.deltaTime * moveSpeed * horizontalInput);
+        if (myFeet.IsTouchingLayers(LayerMask.GetMask("Foreground")))
+        {
+            if (Input.GetButtonDown("Jump"))
+            {
+                Vector2 jumpVelocity = new Vector2(0f, jumpSpeed);
+                myRigidbody2D.velocity += jumpVelocity;
+            }
+        }
+
     }
 
     private void FlipSprite()
@@ -47,4 +62,11 @@ public class PlayerMovement : MonoBehaviour
             transform.localScale = new Vector2(Mathf.Sign(myRigidbody2D.velocity.x), 1f);
         }
     }
+
+    //private void Move()
+    //{
+    //    horizontalInput = Input.GetAxis("Horizontal");
+    //    transform.Translate(Vector2.right * Time.deltaTime * moveSpeed * horizontalInput);
+    //}
+
 }
