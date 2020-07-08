3a715761e9cdcac81e14f7eb86ab355d2b8a75e8
Greenrepeat <61007678+Greenrepeat@users.noreply.github.com>
2020-07-08 10:22:26 +0800

    Ivan's Updated Script

--- Gyro's Daily Routine/Assets/Scripts/PlayerMovementIvan.cs
+++ Gyro's Daily Routine/Assets/Scripts/PlayerMovementIvan.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
+public class PlayerMovementIvan : MonoBehaviour
 {
 
     public CharacterController2D controller;
